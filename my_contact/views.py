31d2f4e9b35e40397cdd3b85fc24f470291839a5
= <=>
2025-07-10 15:42:08 +0530

    inital commit

--- my_contact/views.py
+++ my_contact/views.py
@@ -10,25 +10,30 @@ from django.core.mail import send_mail ,EmailMessage
 
 # Create your views here.
 class ContactLogView(APIView):
+    def get(self, request):
+        return Response({"message" : "You need to take permission from manoj To access This API"})
     def post(self, request):
         contact_serializer = ContactSerializer(data=request.data)
         try: 
             if contact_serializer.is_valid():
                 email_from = settings.EMAIL_HOST_USER
-                name = request.data['name']
-                user_email =  request.data['email']
-                phone =  request.data['phone']
-                body =  request.data['message']
+                name = request.data.get('name') 
+                user_email =  request.data.get('email')
+                phone =  request.data.get('phone')
+                body =  request.data.get('message')
                 subject = f"New Contact Form Submission from {name}"
                 message = f'''Name: {name}
                         Email: {user_email}
                         Phone: {phone}
                         Message:{body}'''
                 recipient_list= ["positive.mind.123456789@gmail.com"]
-                email = EmailMessage(subject, message, email_from, recipient_list)
-                print(name, email, body, phone)
-                email.send()
                 contact_serializer.save()
+                try:
+                    email = EmailMessage(subject, message, email_from, recipient_list)
+                    print(name, email, body, phone)
+                    email.send()
+                except:
+                    pass
                 return Response({"msg": "Message Send Sucessfully"}, status=status.HTTP_200_OK)
             else:
                 return Response({"error": "Some Error Occured"}, status=status.HTTP_400_BAD_REQUEST)
--- my_projects/admin.py
+++ my_projects/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import Project
+# from django.contrib import admin
+# from .models import Project
 
-class ProjectAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
+# class ProjectAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Project, ProjectAdmin)
+# admin.site.register(Project, ProjectAdmin)
